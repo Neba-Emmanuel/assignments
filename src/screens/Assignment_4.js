c949a058840775e6c9054ab4d2c1f4b724fe593f
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-16 11:59:53 +0100

    Create Assignment_4.js

---
+++ src/screens/Assignment_4.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Assignment_4 = () => {
+    return(
+        <View>
+            <View></View>
+            <View></View>
+            <View></View>
+            <View></View>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+})
+
+export default Assignment_4;
