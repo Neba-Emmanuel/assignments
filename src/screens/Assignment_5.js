048be446e272442a53fecf9c4635b304d736d04b
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-16 13:26:09 +0100

    Create Assignment_5.js

---
+++ src/screens/Assignment_5.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View, StyleSheet } from 'react-native';
+
+const Assignment_5 = () => {
+    return(
+        <View>
+            <Text>Assignment_5</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Assignment_5;
